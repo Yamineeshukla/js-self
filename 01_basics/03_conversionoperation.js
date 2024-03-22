2117a12f2035f5969242d72b6175eca1f1359542
Yamineeshukla <132455509+Yamineeshukla@users.noreply.github.com>
2024-03-22 07:22:47 +0000

    container config done

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,31 @@
+let score ="33abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueIntNumber = Number(score)
+console.log(typeof valueIntNumber);
+console.log(valueIntNumber);//output NAN
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0
+
+let isLoggedIn =1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+let isLoggdIn =""
+
+let booleanIsLoggdIn=Boolean(isLoggdIn)
+console.log(booleanIsLoggdIn);
+//1=>true; 0=>false
+//"" =>false
+//"hitesh"=>true
+
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//*********************Operations *****************
