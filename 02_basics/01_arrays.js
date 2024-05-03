56809bbe8dcce8fc6703d5620c07243f4e94a4de
Yamineeshukla <132455509+Yamineeshukla@users.noreply.github.com>
2024-05-03 15:59:55 +0000

    containter config done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,13 @@
+//array
+
+const myArr = [0,1,2,3,4,5]
+const myHerors=["shaktiman","naagraj"]
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[0]);
+
+//Array methods
+
+myArr.push(6)
+myArr.push(7)
+myArr.push(8)
+console.log(myArr);
